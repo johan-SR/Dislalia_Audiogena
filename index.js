685930727badcd14ecad3a798f71d418740af57e
Nicolas <josalaza@bancolombia.com.co>
2022-05-23 12:14:23 -0500

    Virtual environment

---
+++ index.js
@@ -0,0 +1,166 @@
+//Movimiento
+AFRAME.registerComponent('static-movement', {
+    schema: {default: ''},
+    init: function () {
+       var el = this.el;
+       el.addEventListener('click', function () {
+          document.querySelector('#player').setAttribute('position', el.getAttribute('position'));
+          console.log("Click: Player moved");
+       });
+    }
+});
+
+AFRAME.registerComponent('collider', {
+   init: function(){
+      this.el.addEventListener('collide', function(e) {
+
+         const idTrigger = e.detail.body.el.id;
+         const triggers = {
+            'trigger': () =>{
+               let audio = document.querySelector("#A1");
+               audio.play();
+               e.detail.body.el.parentNode.removeChild(e.detail.body.el);
+            },
+            'trigger1': () =>{
+               let audio = document.querySelector("#A2");
+               audio.play();
+               e.detail.body.el.parentNode.removeChild(e.detail.body.el);
+               
+            },
+            'trigger2': () =>{
+               let audio = document.querySelector("#A3");
+               audio.play();
+               e.detail.body.el.parentNode.removeChild(e.detail.body.el);
+            },
+            'trigger3': () =>{
+               let audio = document.querySelector("#A3");
+               audio.play();
+               e.detail.body.el.parentNode.removeChild(e.detail.body.el);
+            }
+            /*,'trigger4': () =>{
+               var luz = document.querySelector("#luz");
+               luz.setAttribute("light", "color: grey");
+               e.detail.body.el.parentNode.removeChild(e.detail.body.el);
+            },
+            'trigger5': () =>{
+               var model = document.querySelector("#MonstruoPasillo");
+               model.setAttribute("visible", "true");
+               model.setAttribute("animation", "property: position; dur: 3000; to: 35 0.1 -5; autoplay: true;");
+               model.setAttribute("sound", "src:#A5; autoplay: true;");
+               setTimeout(function(){
+                  model.parentNode.removeChild(model);
+               },3000);
+               e.detail.body.el.parentNode.removeChild(e.detail.body.el);
+            },
+            'trigger6': () =>{
+               var model = document.querySelector("#MonstruoPasillo1");
+               model.setAttribute("visible", "true");
+               model.setAttribute("animation", "property: position; dur: 3000; to: -10 0 22; autoplay: true;");
+               model.setAttribute("sound", "src:#A6; autoplay: true;");
+               setTimeout(function(){
+                  model.parentNode.removeChild(model);
+               },3000);
+               e.detail.body.el.parentNode.removeChild(e.detail.body.el);
+            }*/
+         }
+         triggers[idTrigger] ? triggers[idTrigger](): null;
+      });
+   }
+})
+var animationG=true;
+function animationGhost(){
+   if(animationG){
+      var model = document.querySelector("#ghostM");
+      model.setAttribute("animation", "property: position; dur: 3000; to: -6 8 -16; autoplay: true;");
+      model.setAttribute("sound", "src:#A7; autoplay: true;");
+      setTimeout(function(){
+         model.parentNode.removeChild(model);
+      },3000);
+      animationG=false;
+   }
+}
+function empezar(){
+   var player = document.querySelector("#player");
+   player.setAttribute("position", "0 0 -1");
+   player.setAttribute("sound", "src: #Fondo; autoplay: true");
+   var luz = document.querySelector("#luz");
+   luz.setAttribute("light", "intensity: 1");
+}
+
+let contador =0;
+let a = aasd;
+function bien(){
+   let muro = document.querySelector("#muro");
+   muro.parentNode.removeChild(muro);
+   let trabajar = document.querySelector("#trabajarOpc")
+   trabajar.parentNode.removeChild(trabajar);
+   let tradajar = document.querySelector("#tradajarOpc")
+   tradajar.parentNode.removeChild(tradajar);
+   contador += 10;
+   console.log(contador)
+}
+function mal(){
+   let muro = document.querySelector("#muro");
+   muro.parentNode.removeChild(muro);
+   let trabajar = document.querySelector("#trabajarOpc")
+   trabajar.parentNode.removeChild(trabajar);
+   let tradajar = document.querySelector("#tradajarOpc")
+   tradajar.parentNode.removeChild(tradajar);
+}
+
+function bienCo(){
+   let muro1 = document.querySelector("#muro1");
+   muro1.parentNode.removeChild(muro1);
+   let cobija = document.querySelector("#cobijaOpc")
+   cobija.parentNode.removeChild(cobija);
+   let codija = document.querySelector("#codijaOpc")
+   codija.parentNode.removeChild(codija);
+   contador += 10;
+   console.log(contador)
+}
+function malCo(){
+   let muro1 = document.querySelector("#muro1");
+   muro1.parentNode.removeChild(muro1);
+   let cobija = document.querySelector("#cobijaOpc")
+   cobija.parentNode.removeChild(cobija);
+   let codija = document.querySelector("#codijaOpc")
+   codija.parentNode.removeChild(codija);
+}
+
+function bienA(){
+   let muro2 = document.querySelector("#muro2");
+   muro2.parentNode.removeChild(muro2);
+   let amistad = document.querySelector("#amistadOpc")
+   amistad.parentNode.removeChild(amistadOpc);
+   let amitad = document.querySelector("#amitadOpc")
+   amitad.parentNode.removeChild(amitad);
+   contador += 10;
+   console.log(contador)
+}
+function malA(){
+   let muro2 = document.querySelector("#muro2");
+   muro2.parentNode.removeChild(muro2);
+   let amistad = document.querySelector("#amistadOpc")
+   amistad.parentNode.removeChild(amistadOpc);
+   let amitad = document.querySelector("#amitadOpc")
+   amitad.parentNode.removeChild(amitad);
+}
+
+function bienCo1(){
+   let muro3 = document.querySelector("#muro3");
+   muro3.parentNode.removeChild(muro3);
+   let cobija1 = document.querySelector("#cobijaOpc1")
+   cobija1.parentNode.removeChild(cobija1);
+   let codija1 = document.querySelector("#codijaOpc1")
+   codija1.parentNode.removeChild(codija1);
+   contador += 10;
+   console.log(contador)
+}
+function malCo1(){
+   let muro3 = document.querySelector("#muro3");
+   muro3.parentNode.removeChild(muro3);
+   let cobija1 = document.querySelector("#cobijaOpc1")
+   cobija1.parentNode.removeChild(cobija1);
+   let codija1 = document.querySelector("#codijaOpc1")
+   codija1.parentNode.removeChild(codija1);
+}
